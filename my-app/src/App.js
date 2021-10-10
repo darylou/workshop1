2509c35855ecbcb14e128c648b4b7aa39fd356e7
Daryl Ou <darylou@169-231-170-96.wireless.ucsb.edu>
2021-10-09 21:14:30 -0700

    UPDATEgit add App.jsgit add App.jsgit add App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload NO!.
+          Edit <code>src/App.js</code> and save to reload NO! MAYBE!.
         </p>
         <a
           className="App-link"
